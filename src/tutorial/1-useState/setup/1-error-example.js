f163ef42d7705080c1d9b3eb2acc5447bad4733f
Prasad Surase <prasad.surase@coupa.com>
2021-04-25 11:07:57 +0530

    example without using useState

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = 'hello world';
+
+  const handleClick = () => {
+    console.log(title);
+    title = "hola amigo";
+  }
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <button type='button' className="btn" onClick={handleClick}>Change title</button>
+    </div>
+  );
 };
 
 export default ErrorExample;
