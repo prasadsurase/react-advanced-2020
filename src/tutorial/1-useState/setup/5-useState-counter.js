1dead3b6f6f35d39c4556e41a5c5324f9bda68aa
Prasad Surase <prasad.surase@coupa.com>
2021-04-26 18:46:06 +0530

    example with using useState on counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -7,6 +7,14 @@ const UseStateCounter = () => {
     setValue(val);
   }
 
+  const complexIncrement = () => {
+    setTimeout(() => {
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 3000);
+  }
+
   return (
     <React.Fragment>
       <section>
@@ -15,6 +23,14 @@ const UseStateCounter = () => {
         <button className="btn" type="button" onClick={() => changeValue(0)}>reset</button>
         <button className="btn" type="button" onClick={() => changeValue(value + 1)}>increment</button>
         <button className="btn" type="button" onClick={() => changeValue(value - 1)}>decrement</button>
+      </section>
+      <section>
+        <h2> Complex counters </h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={complexIncrement}>
+          increase later
+        </button>
+        
       </section>
     </React.Fragment>
   )
