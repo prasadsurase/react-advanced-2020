003b00f4d3f05b996c5481f55201af7591e5a781
Prasad Surase <prasad.surase@coupa.com>
2021-04-26 20:24:56 +0530

    example with using basic useEffect

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,18 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    document.title = `Hello world ${value}`
+  });
+
+  return (
+    <React.Fragment>
+      <h3>{value}</h3>
+      <button className='btn' type='button' onClick={() => setValue(value + 1)}>click me!</button>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectBasics;
