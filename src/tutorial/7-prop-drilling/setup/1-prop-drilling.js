85a158e2ff4bd997f4c1c93b712e803e219cce0a
Prasad Surase <prasad.surase@coupa.com>
2021-04-27 20:36:37 +0530

    example of passing functions as props without useContext

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,49 @@
 import React, { useState } from 'react';
+import { data } from '../../../data';
 
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people, setPeople] = useState(data);
+
+  const removePerson =(id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  }
+
+  return (
+    <React.Fragment>
+      <h2>Prop Drilling</h2>
+      <h3>List of People</h3>
+      <List people={people} removePerson={removePerson}/>
+    </React.Fragment>
+  );
 };
+
+const List = (props) => {
+  const {people} = props;
+
+  return(
+    <div className="items">
+      {people.map((person) => {
+        return <Person key={person.id} person={person} removePerson={props.removePerson}/>
+      })}
+    </div>
+  );
+};
+
+const Person = (props) => {
+  const {id, name} = props.person;
+
+  return (
+    <React.Fragment>
+      <div className="item">
+        <h4>{name}</h4>
+        <button type="button" className="btn" onClick={() => props.removePerson(id)}>remove</button>
+      </div>
+    </React.Fragment>
+  );
+}
 
 export default PropDrilling;
