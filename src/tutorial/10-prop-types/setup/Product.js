a8ae60b0f60b9b9eb42989e13441789025778a7d
Prasad Surase <prasad.surase@coupa.com>
2021-04-28 02:57:34 +0530

    example of proptypes and validtions

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import defaultImage from '../../../assets/default-image.jpeg';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = (props) => {
+  const {name, image, price} = props;
+  return (
+    <article className="product">
+      <img src={image?.url} alt={name}/>
+      <h4>{name}</h4>
+      <h5>{price}</h5>
+    </article>
+  );
 };
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number
+}
+
+Product.defaultProps = {
+  name: 'default name',
+  image: {url: defaultImage},
+  price: 0
+}
 
 export default Product;
