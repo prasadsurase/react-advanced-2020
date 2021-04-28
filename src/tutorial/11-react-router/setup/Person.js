fc9ba84007b40ef1ed1b30db3e6d3e42e1c0ea87
Prasad Surase <prasad.surase@coupa.com>
2021-04-28 09:14:50 +0530

    example of react routers

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -2,9 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { data } from '../../../data';
 import { Link, useParams } from 'react-router-dom';
 const Person = () => {
+  const {id} = useParams();
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    const person = data.find((person) => person.id === parseInt(id));
+    setName(person.name);
+  }, []);
+
   return (
     <div>
-      <h2>person</h2>
+      <h2>{name}</h2>
     </div>
   );
 };
